844968c84666ea7e9d7014b22481cf9b3fe2dfcb
Mohamed Moafa <alfars4735@gmail.com>
2021-04-23 19:03:37 +0300

    circle area

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CircleArea
+{
+    class Program
+    {
+
+        public static void Print(string text,ConsoleColor c)
+        {
+            Console.ForegroundColor = c;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+        static void Main()
+        {
+            double radius = 0;
+            Print("=> Circle Area By Mohamed Moafa", ConsoleColor.DarkGreen);
+            Console.WriteLine("".PadLeft(Console.BufferWidth,'-'));
+            Console.Write("Enter Radius: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            radius = double.Parse(Console.ReadLine());
+            Console.ResetColor();
+            Console.WriteLine($"Circle area is = {radius*radius*Math.PI:f2}"); 
+            Console.ReadKey();
+        }
+    }
+}
